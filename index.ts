45366ea36a938e2d7f55e175fe55ddb4944cc134
Noam Loewenstern <noamloewenstern@gmail.com>
2024-08-16 01:35:18 +0300

    feat: hot reload of src-dir of extension

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { generateCrx } from './lib/crx/compile';
 import { generateExtensionId, generatePublicKey } from './lib/crx/utils';
 import { initServer, serveServer } from './server';
 import pDebounce from 'p-debounce';
+import { watch } from 'node:fs';
 
 async function main() {
   const args = parseArgs();
@@ -11,13 +12,11 @@ async function main() {
     console.log('compiling Crx');
     await generateCrx(args);
   }
-
+  const compileCrx = pDebounce(() => generateCrx(args), 1000);
   const app = initServer({
     staticDir: args.dstDir,
     log: true,
-    onInvokeForceCompile: pDebounce(async () => {
-      await generateCrx(args);
-    }, 500),
+    onInvokeForceCompile: compileCrx,
   });
 
   console.log('initiated server');
@@ -28,6 +27,14 @@ async function main() {
   const hostname = args.hostname || '0.0.0.0';
   console.log(`Starting server on: http://${hostname}:${args.port}, and tunnel via ${args.updateUrl}`);
   serveServer({ app, hostname, port: args.port });
+
+  if (args.hotReload) {
+    const watcher = watch(args.srcExtensionDir, () => {
+      console.log(`srcExtension directory changed! recompiling crx...`);
+      void compileCrx();
+    });
+    watcher.unref();
+  }
 }
 
 main();
